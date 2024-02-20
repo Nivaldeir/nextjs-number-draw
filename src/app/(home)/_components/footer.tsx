3e3ffbbba8444a55e2dea87e39104f7376455cb6
Nivaldeir Silva <nivaldeir-silva@hotmail.com>
2024-02-20 15:43:59 -0300

    feat: add Footer components

---
+++ src/app/(home)/_components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    
+      <div className="absolute bottom-0">
+        <div className="w-screen text-center h-[10%]">
+          Desenvolvido por nivaldeir-silva@hotmail.com
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
